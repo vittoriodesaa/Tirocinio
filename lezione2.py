bcb8eff759dd9f03fca1c5687e1cad080ef67f61
Vittorio De Santis <vittoriodesantis@MacBook-Air-di-Vittorio.local>
2026-04-15 15:25:57 +0200

    Primo commit: Setup progetto Tirocinio e Lezione 1

---
+++ lezione2.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langgraph.graph import StateGraph, MessagesState, START, END
+
+# carico le chiavi
+load_dotenv()
+
+# inizializzo ai
+llm = ChatGroq(model="llama-3.1-8b-instant")
+
+
+def trolliamo(stato: MessagesState):
+    risposta_vera = llm.invoke(stato['messages'])
+    
+    return {"messages": [risposta_vera]}
+
+
+graph = StateGraph(MessagesState)
+graph.add_node('trolliamo', trolliamo)
+graph.add_edge(START, 'trolliamo')
+graph.add_edge('trolliamo', END)
+
+graph = graph.compile()
+
+
+ris = graph.invoke({'messages': [{'role': 'user', 'content': 'Uè, spiegami cos è un grafo in 5 parole'}]})
+
+# print ultima frase detta dall'ia
+print(ris['messages'][-1].content)
